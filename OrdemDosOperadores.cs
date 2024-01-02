255d62646ceecb70c8b76fb0f74d9c991ffc1c3e
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-01-02 15:04:38 -0300

    Create OrdemDosOperadores.cs

---
+++ OrdemDosOperadores.cs
@@ -0,0 +1,15 @@
+using ExemploFundamentos.Models;
+
+// Ordem dos Operadores 
+
+double a = 4/(2 + 2);
+/**
+Mesma ordem das operações matemáticas 
+primeiro parênteses 
+depois expoentes 
+divisão 
+multiplicação
+adição 
+subtração
+*/
+Console.WriteLine("A: " + a);
