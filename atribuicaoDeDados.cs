92a2bca1dc270a075c09d1f52fb7223ed9947446
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-01-02 14:10:04 -0300

    Create atribuicaoDeDados.cs

---
+++ atribuicaoDeDados.cs
@@ -0,0 +1,15 @@
+ using ExemploFundamentos.Models; 
+
+int a = 20; 
+int b = 30; 
+
+int c = a+b;
+
+c += 5; // c = c + 5;
+Console.WriteLine("C: " + c);
+
+c-= 10; // = c - 10;
+
+Console.WriteLine("A: " + a); 
+Console.WriteLine("B: " + b); 
+Console.WriteLine("C: " + c);
