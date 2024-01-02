bc85d17322462a6bd4b7f1dfd36e3182740e3bfb
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-01-02 14:22:33 -0300

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,7 @@
+using ExemploFundamentos.Models;
+
+
+// int a = Convert.ToInt32 ("5"); - Casting utilizando CONVERT
+
+int a = int.Parse("5");// casting utilizando Parse
+Console.WriteLine("A: " + a);
