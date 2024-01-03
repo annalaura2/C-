2a75b81b14ea94fc4286f23a961da666f63c8a98
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-01-03 09:44:26 -0300

    Create SwitchCasee.cs

---
+++ SwitchCasee.cs
@@ -0,0 +1,19 @@
+using ExemploFundamentos;
+
+//SWITCH CASE 
+
+Console.WriteLine("Informe uma letra"); 
+string letra = Console.ReadLine();
+
+switch(letra){
+    case "a":
+    case "e":
+    case "i": 
+    case "o": 
+    case "u":
+        Console.WriteLine("Vogal");
+        break; 
+    default:
+        Console.WriteLine("Não é vogal");
+        break;
+}
