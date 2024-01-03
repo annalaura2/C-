11cfc087b1ca93d5478fbe8ab2d11bc664445873
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-01-03 10:08:26 -0300

    Create operadorOr.cs

---
+++ operadorOr.cs
@@ -0,0 +1,15 @@
+using ExemploFundamentos;
+
+//Operadores Lógicos 
+// OR => "ou" 
+
+bool ehMaiorDeIdade = true; 
+bool autorizacao = true; 
+
+if(ehMaiorDeIdade || autorizacao){
+    Console.WriteLine("Entrada permitida");
+}else{
+    Console.WriteLine("Entrada negada");
+}
+
+
