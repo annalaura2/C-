d02ac79f3ac3ffb305eb24d57db6ca8385838e02
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2024-01-03 10:11:28 -0300

    Create OperadorAND.cs

---
+++ OperadorAND.cs
@@ -0,0 +1,14 @@
+using ExemploFundamentos;
+
+//Operadores Lógicos 
+// OR => "ou" ||
+// AND => "e" &&
+
+bool presencaM = true; 
+bool mediaT = false; 
+
+if(presencaM && mediaT){
+    Console.WriteLine("Aprovado");
+}else{
+    Console.WriteLine("Reprovado");
+}
